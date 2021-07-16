6657346f6f27bca6ca42221310589566c4ce9b83
marencoalf <alfredomarenco@boletea.com>
2021-07-16 01:45:30 -0500

    Agregamos Node

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -1,4 +1,4 @@
-<x-applayout>
+<x-app-layout>
     <section class="bg-cover" style="background-image: url({{ asset('img/courses/portada2.jpg') }});">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-36">
             <div class="w-full lg:w-1/2 md:w-3/4">
@@ -11,4 +11,4 @@
     </section>
 
     @livewire('courses-index')
-</x-applayout>
+</x-app-layout>
