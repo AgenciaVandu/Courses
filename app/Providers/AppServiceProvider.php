b36ece50aa8a820f2c193271657d0d9dad71cab2
marencoalf <alfredomarenco@boletea.com>
2021-07-16 01:35:27 -0500

    Agregamos Node

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        Schema::defaultStringLength(250);
         Lesson::observe(LessonObserver::class);
         Section::observe(SectionObserver::class);
 
